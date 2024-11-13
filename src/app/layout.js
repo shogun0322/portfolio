d464e935d33db5e83d0c3e52ed8da255b7d864f3
panuwat <panuwat.suw@skynebula.tech>
2024-11-14 03:12:17 +0700

    Recheck image

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,10 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Panuwat Suwanritdej",
+  description: "Portfolio Panuwat.suw",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">

7a5762c9930bea565cdc87253bd2ea76a82c7985
panuwat <panuwat.suw@skynebula.tech>
2024-11-17 23:46:47 +0700

    Check ana

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+"use client";
+import { useEffect } from "react";
+import { trackPageView } from "@/lib/analytics";
+
 import About from "@/components/about";
 import Intro from "@/components/intro";
 import More from "@/components/more";
 import Welcome from "@/components/welcome";
 
 export default function Home() {
+  useEffect(() => {
+    trackPageView("port view");
+  }, []);
   return (
     <>
       <Welcome />

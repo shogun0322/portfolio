7e76cacb9a7550b7e52dda437d83dcc9edc258f8
Panuwat <shogun0322@hotmail.com>
2025-06-14 00:13:50 +0700

    Install new project

--- src/lib/analytics.js
+++
@@ -1,7 +0,0 @@
-import { analytics, logEvent } from "./firebaseClient";
-
-export const trackPageView = (path) => {
-  if (analytics && typeof window !== "undefined") {
-    logEvent(analytics, "page_view", { page_path: path });
-  }
-};

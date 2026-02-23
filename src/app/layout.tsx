d7f55658638efb74e096d8b9cc54850dc2293ed9
panuwat <shogun0322@hotmail.com>
2026-02-23 14:16:51 +0700

    Update tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
-import "./globals.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import './globals.css';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Panuwat Suwanritdej",
-  description: "Panuwat Portfolio",
+  title: 'Panuwat Suwanritdej',
+  description: 'Panuwat Portfolio',
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} antialiased`}>
@@ -24,6 +25,21 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+
+      {/* Google Analytics Script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6DDSK2K9B7"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6DDSK2K9B7');
+        `}
+      </Script>
     </html>
   );
 }
